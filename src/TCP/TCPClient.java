f4e3f92c2318ba3ab57a81a989980172081e129b
DIDER <Simon.dider@gmail.com>
2025-09-16 09:44:47 +0200

    Tilføjelse af TCP server

---
+++ src/TCP/TCPClient.java
@@ -0,0 +1,14 @@
+package TCP;
+
+import java.net.Socket;
+
+public class TCPClient {
+
+	public static void main(String argv[]) throws Exception {
+
+		Socket clientSocket = new Socket("10.10.138.66", 6789);
+
+		(new TCPSendThread(clientSocket)).start();
+		(new RecievingThread(clientSocket)).start();
+	}
+}
